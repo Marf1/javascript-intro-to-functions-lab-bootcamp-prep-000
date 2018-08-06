c2efdfdb2f5b67ba92d704cc4848ff7162b2639b
alex.marfleet@gmail.com <alex.marfleet@gmail.com>
2018-08-06 15:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function whisper(string) {
  return string.toLowerCase() 
 }
 
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase
