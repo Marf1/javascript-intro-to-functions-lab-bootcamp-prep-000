f7fdc950cb0da1e4b007b144d460c01558be1426
alex.marfleet@gmail.com <alex.marfleet@gmail.com>
2018-08-06 15:18:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+ return string.toLowerCase() 
+}
+
+var uppercase = "HELLO!"
+var lowercase = 'hello!'
+
+function sayHiToGrandma(string) {
+  if string.toUpperCase() === uppercase
+  console.log()
